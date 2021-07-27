4f8f070a54f6c001c2fa457d2b84f44165376366
Parker Anderson <parker.anderson0810@outlook.com>
2021-07-27 12:21:27 -0400

    "Properties for Battery Object"

--- RevolveApp/src/main/java/com.revolve.models/Battery.java
+++ RevolveApp/src/main/java/com.revolve.models/Battery.java
@@ -1,5 +1,40 @@
 package com.revolve.models;
 
 public class Battery{
+    private String ModelName;
+    private String id;
+    private double BatteryWeightInKg;
+    private decimal CurrentTotalValue;
 
+    public String getModelName() {
+        return ModelName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public double getBatteryWeightInKg() {
+        return BatteryWeightInKg;
+    }
+
+    public decimal getCurrentTotalValue() {
+        return CurrentTotalValue;
+    }
+
+    public void setModelName(String modelName) {
+        ModelName = modelName;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setBatteryWeightInKg(double batteryWeightInKg) {
+        BatteryWeightInKg = batteryWeightInKg;
+    }
+
+    public void setCurrentTotalValue(decimal currentTotalValue) {
+        CurrentTotalValue = currentTotalValue;
+    }
 }
